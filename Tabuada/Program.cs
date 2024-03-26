38943adfe94654baaed2f97d59b01fc5b8d7776f
Usuario <Usuario@CPD03-M08>
2024-03-26 11:29:31 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a tabuada: ");
+            int tab = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número de vezes: ");
+            int vezes = int.Parse(Console.ReadLine());
+
+            int cont = 1;
+
+            while (cont <= vezes)
+            {
+                int resultado = tab * cont;
+                Console.WriteLine(tab + "X" + cont + "=" + resultado);
+                cont ++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
